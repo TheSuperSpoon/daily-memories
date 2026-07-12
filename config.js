9dab24f7e6bdc2746b8e921e8d422b28994c9364
Steven-Qu-04 <2243387748@qq.com>
2026-07-12 13:29:13 +0800

    build(frontend): bundle Supabase SDK and public config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__APP_CONFIG__ = Object.freeze({
+  supabaseUrl: "https://wwjltwdxhwjsoyhiuwcl.supabase.co",
+  supabasePublishableKey: "sb_publishable_BVCSP7k-ifd_d3Rg_cEWpA_M1rL9KFZ",
+  spaceId: "00000000-0000-0000-0000-000000000001",
+});
