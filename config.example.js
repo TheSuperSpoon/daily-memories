2b0eaba031bbebf1f5e252e90d01825c18485efa
Steven-Qu-04 <2243387748@qq.com>
2026-07-12 11:19:33 +0800

    Revert "feat(auth): add account and session UI"
    This reverts commit 7d33124ece69537517ae40b84fd57d56f743c425.

--- config.example.js
+++
@@ -1,5 +0,0 @@
-window.__APP_CONFIG__ = {
-  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
-  supabasePublishableKey: "YOUR_PUBLISHABLE_KEY",
-  spaceId: "00000000-0000-0000-0000-000000000001"
-};
